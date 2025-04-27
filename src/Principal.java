18f7bc3bf4a94d5cc7fa12c9c2472b644eed6365
Humberto Coronado <betocoronado8370@gmail.com>
2025-04-26 22:22:51 -0600

    se agrega el proyecto

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Principal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("what is your name? ");
+        String name = scanner.nextLine();
+        System.out.println("What is your number? ");
+        int number = scanner.nextInt();
+
+        System.out.println("Your name is "+name + " your number is "+ number);
+    }
+
+
+
+
+
 }
