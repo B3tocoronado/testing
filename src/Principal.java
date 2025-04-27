1ad8f5ba19c7d46fb205beafc626f7fe6c644678
betocoronado <betocoronado8370@gmail.com>
2025-04-26 23:43:51 -0600

    nuevo mensxaje de funcionalidad

--- src/Principal.java
+++ src/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        // nueva funcionalidad
 
         System.out.println("what is your name? ");
         String name = scanner.nextLine();
