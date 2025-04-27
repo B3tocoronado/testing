22c3b00420d26846ca91ae0ab20071ebd4026ece
betocoronado <betocoronado8370@gmail.com>
2025-04-26 22:36:00 -0600

    se agrego un mensaje

--- src/Principal.java
+++ src/Principal.java
@@ -4,6 +4,8 @@ public class Principal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+
+        // este es un mensaje nuevo
         System.out.println("what is your name? ");
         String name = scanner.nextLine();
         System.out.println("What is your number? ");
