00ce5858ccec96d75d6e26965965093e010f2298
betocoronado <betocoronado8370@gmail.com>
2025-04-26 23:47:15 -0600

    nuevo mensxaje de funcionalidad

--- src/Principal.java
+++ src/Principal.java
@@ -4,13 +4,11 @@ public class Principal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // nueva funcionalidad
-
         System.out.println("what is your name? ");
         String name = scanner.nextLine();
         System.out.println("What is your number? ");
         int number = scanner.nextInt();
-
+        //a partir de aqui empieza la nueva funcionalidad
         int i=0;
         while (i < number ){
             System.out.println(name+" ERES UN GAY");
